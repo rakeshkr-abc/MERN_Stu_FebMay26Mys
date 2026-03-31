394de3124612e31d80c5f1434651f04cd9fb55c8
rakeshkr-abc <rakesh.kr@abc.courses>
2026-03-31 17:13:13 +0530

    Week 7 example classroom code

---
+++ W7/D1/P3.js
@@ -0,0 +1,25 @@
+// Handling different HTTP methods in express
+const express = require("express");
+
+const app = express();
+
+//To read
+app.get("/users",function(req,res){
+    res.status(200).json([{message:"Success"},
+        {id:1,name:"Rakesh"},
+        {id:2,name:"Bipin"},
+        {id:3,name:"Keerthi"},
+    ]);
+    // res.send("Returning all Users");
+});
+
+//To create
+app.post("/users",function(req,res){
+    //res.status() sets the HTTP status code before 
+    // sending the response body
+    res.status(201).send("User created.");
+});
+
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
---
+++ W7/D2/P2.js
@@ -0,0 +1,21 @@
+// Third-party middleware are available @ npm registry
+const express = require("express");
+//Middlware to log all the requests 
+const morgan = require("morgan");
+
+const cors = require("cors");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+
+app.get("/",function(req,res){
+    res.json({
+        message: "Third-party middleware executed before this response"
+    });
+});
+
+app.listen(4000,function(){
+    console.log("Express server running at http://168.1.3.28:4000");
+});
