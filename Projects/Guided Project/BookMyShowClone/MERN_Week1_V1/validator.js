d031c669ecf0718a1c780fe003aa1155334a93b2
rakeshkr-abc <rakesh.kr@abc.courses>
2026-04-06 20:28:49 +0530

    Callback based validation functions

--- Projects/Guided Project/BookMyShowClone/MERN_Week1_V1/validator.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V1/validator.js
@@ -10,17 +10,21 @@ function validateMovieSelection(movies,movieId,callback) {
 
 }
 
-function validateTimeSelection(movie,selectedTime,callback) {
-    const selectedShowtime = movie.showtimes.find((show)=>show.time.toLowerCase()===selectedTime.toLowerCase());
+function validateTimeSelection(movie, selectedTime, callback) {
+  const selectedShowtime = movie.showtimes.find(
+    (show) => show.time.toLowerCase() === selectedTime.toLowerCase()
+  );
 
-    if (!selectedShowtime) {
-        return callback("Invalid time slot selection.Choose a valid time slot.",null);
-    }
-    callback(null,selectedShowtime);
+  if (!selectedShowtime) {
+    return callback("Invalid time slot selection. Please choose a valid show time.", null);
+  }
+
+  callback(null, selectedShowtime);
 }
 
+
 function validateSeatCount(seatCount,callback) {
-    if (!isNaN(seatCount) || seatCount <= 0) {
+    if (isNaN(seatCount) || seatCount <= 0) {
         return callback("Invalid seat count.Enter a valid seat count.",null);
     }
     callback(null,seatCount);
