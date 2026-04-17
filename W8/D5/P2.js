680a523e4e702a9f7268397ee0872e5db9938716
rakeshkr-abc <rakesh.kr@abc.courses>
2026-04-17 12:35:42 +0530

    Multiple file upload using Multer

--- W8/D5/P2.js
+++ W8/D5/P2.js
@@ -30,7 +30,7 @@ async function main() {
     });
     app.listen(3000, () => {
       console.log("Server started on port http://localhost:3000");
-      console.log(" POST /upload-dest");
+      console.log(" POST /upload-multiple");
     });
   } catch (error) {
     console.log("Error:", error.message);
