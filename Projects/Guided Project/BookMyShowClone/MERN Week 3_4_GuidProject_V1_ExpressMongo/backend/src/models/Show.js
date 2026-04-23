3bdbfb2917dfd2ff4b9e88f884b75b0838e878b5
rakeshkr-abc <rakesh.kr@abc.courses>
2026-04-23 11:38:14 +0530

    next is not recognized in new mongoose

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Show.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Show.js
@@ -52,10 +52,16 @@ const showSchema = new mongoose.Schema({
 showSchema.index({movieId:1,date:1});
 
 //Add Validation
-showSchema.pre("save",function(next){
-    if (this.availableSeats>this.totalSeats) {
-        return next(new Error("Available seats cannot exceed total seats"));
+// showSchema.pre("save",function(next){
+//     if (this.availableSeats>this.totalSeats) {
+//         return next(new Error("Available seats cannot exceed total seats"));
+//     }
+//     next();
+// });
+// New Add Validation code
+showSchema.pre("save", async function () {
+    if (this.availableSeats > this.totalSeats) {
+        throw new Error("Available seats cannot exceed total seats");
     }
-    next();
 });
 module.exports = mongoose.model("Show",showSchema);
