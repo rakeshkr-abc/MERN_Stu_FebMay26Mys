7814ceeaf268d32cc5c62b67d165a4ea7f8467b9
rakeshkr-abc <rakesh.kr@abc.courses>
2026-04-28 12:34:45 +0530

    React application to learn Components concept

---
+++ W9/D2/my-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import { FunctionName } from "./components/FunctionalCompOne.jsx"
+import { FunctionalComponentsBasics } from "./components/FunctionalComponentsBasics.jsx";
+import { ClassComponentsBasics } from "./components/ClassComponentsBasics.jsx"
+import { FunctionComp } from "./components/FunctionalComponentsAdv.jsx"
+
+function App() {
+  return (
+    // Fragment in react : <> </>
+    <>
+        {/* Component name */}
+      {/* <FunctionName /> */}
+      {/* <FunctionalComponentsBasics /> */}
+      {/* <ClassComponentsBasics /> */}
+      <FunctionComp />
+    </>
+  )
+}
+export default App
