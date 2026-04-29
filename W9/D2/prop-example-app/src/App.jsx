c449ff58d873b3eeb3da829fdcff08e4256f5d8c
rakeshkr-abc <rakesh.kr@abc.courses>
2026-04-29 14:23:01 +0530

    Passing Functions as Props

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -8,6 +8,7 @@ import { CompositionDemo } from "./components/P6";
 import { ObjectsAndArrayProps } from "./components/P7";
 import { PropsImmutability } from "./components/P8";
 import { PropsState } from "./components/P9";
+import { FunctionProps } from "./components/P10";
 function App() {
   return (
     <>
@@ -19,7 +20,8 @@ function App() {
       {/* <CompositionDemo /> */}
       {/* <ObjectsAndArrayProps /> */}
       {/* <PropsImmutability /> */}
-      <PropsState/>
+      {/* <PropsState/> */}
+      <FunctionProps />
     </>
   )
 }
