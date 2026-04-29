d9f2bc9e3037bb9f1963f410be9f5c95ec801387
rakeshkr-abc <rakesh.kr@abc.courses>
2026-04-29 09:50:31 +0530

    Wrapper component

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { PropBasics } from "./components/P1";
 import { PropDestructuring } from "./components/P2";
+import { PropsChildren } from "./components/P3";
 function App() {
   return (
     <>
       {/* <PropBasics /> */}
-      <PropDestructuring />
+      {/* <PropDestructuring /> */}
+      <PropsChildren />
     </>
   )
 }
