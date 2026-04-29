c436823eb2c9e36369ce70378329c50740fcdce7
rakeshkr-abc <rakesh.kr@abc.courses>
2026-04-29 11:43:44 +0530

    State Lifting

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -3,13 +3,15 @@ import { PropBasics } from "./components/P1";
 import { PropDestructuring } from "./components/P2";
 import { PropsChildren } from "./components/P3";
 import { PropDrillingDemo } from "./components/P4";
+import { SharedStateParent } from "./components/P5";
 function App() {
   return (
     <>
       {/* <PropBasics /> */}
       {/* <PropDestructuring /> */}
       {/* <PropsChildren /> */}
-      <PropDrillingDemo />
+      {/* <PropDrillingDemo /> */}
+      <SharedStateParent/>
     </>
   )
 }
