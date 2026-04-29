62ca6c2b4068cf0130ce1b33772754d7839a5f68
rakeshkr-abc <rakesh.kr@abc.courses>
2026-04-29 12:11:48 +0530

    Comments added

--- W9/D2/prop-example-app/src/components/P5.jsx
+++ W9/D2/prop-example-app/src/components/P5.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import { useState } from 'react';
 // State Lifting: moving shared state up to the 
 // closest common parent
 // We use this:
