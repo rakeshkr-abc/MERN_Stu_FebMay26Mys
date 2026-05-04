54b63ca6e3d1b63f7709e613fad71b5f5a54171b
rakeshkr-abc <rakesh.kr@abc.courses>
2026-05-04 11:44:44 +0530

    Updating objects and Arrays

--- W10/D1/state-hooks-app/src/App.jsx
+++ W10/D1/state-hooks-app/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import { UseStateBasics } from "./components/P1";
+import { UpdatingObjectsArraysState } from "./components/P2";
 function App() {
   return (
     <>
-      <UseStateBasics />
+      {/* <UseStateBasics /> */}
+      <UpdatingObjectsArraysState />
     </>
   )
 }
