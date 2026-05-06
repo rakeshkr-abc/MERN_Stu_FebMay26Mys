3addab4ee2a0172f6eee9d3fe04cb3ac75c5808c
rakeshkr-abc <rakesh.kr@abc.courses>
2026-05-06 14:08:49 +0530

    useCallback hook

--- W10/D2/my-app/src/App.jsx
+++ W10/D2/my-app/src/App.jsx
@@ -3,13 +3,16 @@ import { UseContextIntro } from "./components/P1";
 import { CustomHooksIntro} from "./components/P2";
 import { UserRefIntro} from "./components/P3";
 import { UseReducerIntro } from "./components/P4";
+import { UseCallbackIntro } from "./components/P5";
+
 function App() {
   return (
     <>
       {/* <UseContextIntro />        */}
       {/* <CustomHooksIntro /> */}
       {/* <UserRefIntro /> */}
-      <UseReducerIntro />
+      {/* <UseReducerIntro /> */}
+      <UseCallbackIntro />
     </>
   )
 }
