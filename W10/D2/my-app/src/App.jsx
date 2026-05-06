f2d77fe23b7da35791b82389953151634d3398a9
rakeshkr-abc <rakesh.kr@abc.courses>
2026-05-06 12:17:35 +0530

    Custom Hooks Intro

--- W10/D2/my-app/src/App.jsx
+++ W10/D2/my-app/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import { UseContextIntro } from "./components/P1";
+import { CustomHooksIntro} from "./components/P2";
 function App() {
   return (
     <>
-      <UseContextIntro />       
+      {/* <UseContextIntro />        */}
+      <CustomHooksIntro />
     </>
   )
 }
