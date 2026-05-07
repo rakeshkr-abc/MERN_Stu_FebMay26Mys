92c31076fb85be22967303c740c4c33074aefa80
rakeshkr-abc <rakesh.kr@abc.courses>
2026-05-07 12:32:36 +0530

    useCallback with Event Handlers

--- W10/D3/my-event-app/src/App.jsx
+++ W10/D3/my-event-app/src/App.jsx
@@ -4,7 +4,6 @@ import { EventObject } from "./components/P2";
 import { PassingArguments } from "./components/P3";
 import { UseCallbackEvents } from "./components/P5";
 
-
 function App() {
    return (
     <>
