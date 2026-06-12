44d6a0298b96c06d1e46baff6d49fb3e719f43c1
rakeshkr-abc <rakesh.kr@abc.courses>
2026-06-12 12:04:52 +0530

    Usage of Props

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,10 +1,31 @@
-import './App.css'
-import HomePage from "./pages/HomePage";
+import "./App.css";
+import MovieCard from "./components/MovieCard";
 function App() {
-   return (
-    // Compnent Composition
-    <HomePage />
+  return (
+    <>
+      <MovieCard
+        title="Inception"
+        genre="Sci-fi"
+        rating={4.8}
+        duration="2h 28m"
+        poster="https://picsum.photos/200/300?1"
+      />
+      <MovieCard
+        title="Avatar"
+        genre="Sci-fi"
+        rating={4.1}
+        duration="3h 28m"
+        poster="https://picsum.photos/200/300?2"
+      />
+      <MovieCard
+        title="FightClub"
+        genre="Sci-fi"
+        rating={4.9}
+        duration="2h 28m"
+        poster="https://picsum.photos/200/300?3"
+      />
+    </>
   );
 }
 
-export default App
+export default App;
