e248dd6b645d1a1ac48024d70001dc68ef1851ea
rakeshkr-abc <rakesh.kr@abc.courses>
2026-06-12 11:37:07 +0530

    Single Component, Component Composition, Parent-Child components

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,26 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-
+import HomePage from "./pages/HomePage";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-      <div>
-
-            <h1>
-                BookMyShow Clone
-            </h1>
-
-            <p>
-                Welcome to BookMyShow Frontend.
-            </p>
-
-        </div>
-
-  )
+   return (
+    // Compnent Composition
+    <HomePage />
+  );
 }
 
 export default App
