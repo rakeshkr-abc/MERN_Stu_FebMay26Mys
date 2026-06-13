623729ee995e0ee4713912e6a92e3c5bb2c030f7
rakeshkr-abc <rakesh.kr@abc.courses>
2026-06-13 22:20:08 +0530

    Project Folder renamed

---
+++ W12/Example Frontend Project/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+import MovieExplorer from "./components/MovieExplorer";
+import ErrorBoundary from "./components/ErrorBoundary";
+
+export default function App(){
+  return(
+    <div>
+      <header>
+          <ErrorBoundary>
+              <MovieExplorer />
+          </ErrorBoundary>
+      </header>
+    </div>
+  )
+}
+const styles = {
+  container:{
+    maxWidth: "900px",
+    margin:"0 auto",
+    padding: "20px",
+  },
+  button: {
+    padding: "8px 12px",
+    marginTop:"8px",
+  },
+};
